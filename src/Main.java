1c6520d543272523ded1db8a6e9c865e7576f36a
ll01063598911-lab <ll01063598911@gmail>
2025-11-27 12:06:59 +0900

    배열의 한계(수량을 직접 관리)

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
-//        System.out.println(new Article().id);
+
+       void exam1() {
+           int articleLength =0;
+
+
+        }
+
+        articles[0] = new Article();
+        articleLength++;
+        articles[1] = new Article();
+        articleLength++;
+        articles[2] = new Article();
+        articleLength++;
+        articles[3] = new Article();
+        articleLength++;
+        articles[4] = new Article();
+        articleLength++;
+        articles[5] = new Article();
+        articleLength++;
+
+        for(int i = 0 ; i < articleLength i++) {
+            Article article = articles[];
+
+        }
     }
 
-    Article[] articles = new Article[3];
-    articles[0] = new Article();
-    articles[1] = new Article();
-    articles[2] = new Article();
+        class Article {
+            static int lastId;   //static 초기화 블럭에서 사용됨
+            int id;
 
+            static {             //static 초기화 블럭, 프로그램 시작시 1회 / 메모리 상단에 위치함(제일먼저 실행됨)
+                lastId = 0;
+            }
 
-   for(Article article :abstract){
-        System.out.println(article.id);
+            Article() {             // default 생성자
+                this(lastId + 1);   // this() --> 다른 생성자 호출
+                lastId++;
+            }
+
+            Article(int id) {       //생성자 오버로딩
+                this.id = id;
+            }
+        }
     }
-}
-}
-
-
-class Article {
-    static int lastId;   //static 초기화 블럭에서 사용됨
-    int id;
-
-    static {             //static 초기화 블럭, 프로그램 시작시 1회 / 메모리 상단에 위치함(제일먼저 실행됨)
-        lastId = 0;
-    }
-
-    Article() {             // default 생성자
-        this(lastId + 1);   // this() --> 다른 생성자 호출
-        lastId++;
-    }
-
-    Article(int id) {       //생성자 오버로딩
-        this.id = id;
-    }
-}}
